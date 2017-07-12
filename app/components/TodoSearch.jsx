243702dc245c60aa1d4e88439ad07ea09705d40c
Nan <zn7726@hotmail.com>
2017-07-12 15:09:43 +1000

    add TodoSearchSearch

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -11,10 +11,10 @@ var TodoSearch = React.createClass({
   render: function() {
     return (
       <div>
-        <div><input type="search" ref="searchText" placeholder="Search Todos" onChange="{this.handleSearch}" /></div>
+        <div><input type="search" ref="searchText" placeholder="Search Todos" onChange={this.handleSearch} /></div>
         <div>
           <label>
-            <input type="checkbox" ref="showCompleted" onChange="{this.handleSearch}" />
+            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch} />
             Show completed todos
           </label>
         </div>
